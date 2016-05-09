e453049d5ccf9323fe42ad466b38e15f40ac7896
Aditya <ladwa.aditya@gmail.com>
2016-05-09 13:55:42 +0530

    First Draft : Created packages for dagger and UI

---
+++ app/src/main/java/com/example/aditya/daggerretrofitokhttp_tutorial/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.aditya.daggerretrofitokhttp_tutorial.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.aditya.daggerretrofitokhttp_tutorial.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
